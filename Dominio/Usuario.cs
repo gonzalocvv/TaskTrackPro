46d5ae90517fa17eccac97884a3316c12f3474b4
gonzalocvv <gcabrera3210@gmail.com>
2025-05-02 19:38:55 -0300

    Primer test pasa la prueba

--- Dominio/Usuario.cs
+++ Dominio/Usuario.cs
@@ -2,4 +2,10 @@
 
 public class Usuario
 {
+    
+    public Usuario(string nombre, string apellido, string email, string fechaNacimiento, string contraseña)
+    {
+        if (string.IsNullOrWhiteSpace(nombre))
+            throw new ArgumentException("El nombre no puede estar vacío.");
+    }
 }
