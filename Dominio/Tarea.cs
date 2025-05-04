d0c8fbd63c6a9e14c59f48b0185d49b40998cda2
Nicolas Ruy Lopez <NR256563@fi365.ort.edu.uy>
2025-05-03 23:14:06 -0300

    Primeros test en verde de la clase tarea

--- Dominio/Tarea.cs
+++ Dominio/Tarea.cs
@@ -4,8 +4,20 @@ public class Tarea
 {
     public Tarea(string titulo, string descripcion, string fechaDeInicio, int? duracion)
     {
-        
+        ValidarCamposString(titulo, "El titulo");
+        ValidarCamposString(descripcion, "La descripcion");
+        ValidarCamposInt(duracion, "La duracion");
     }
     
+    private static void ValidarCamposString(string dato, string nombreCampo)
+    {
+        if (string.IsNullOrWhiteSpace(dato))
+            throw new ArgumentException($"{nombreCampo} no puede ser vacío.");
+    }
     
+    private static void ValidarCamposInt(int? dato, string nombreCampo)
+    {
+        if (dato == null)
+            throw new ArgumentException($"{nombreCampo} no puede ser vacío.");
+    }
 }
