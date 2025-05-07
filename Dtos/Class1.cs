7b94fb83beb4d53d30909f73f01062ef1309d8ae
gonzalocvv <gcabrera3210@gmail.com>
2025-05-07 17:47:59 -0300

    saco get y set

--- Dtos/Class1.cs
+++ Dtos/Class1.cs
@@ -1,20 +1,23 @@
 ﻿namespace Dtos;
 
-public class CreateUsuarioDto {
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public string Email { get; set; }
-    public DateTime FechaNacimiento { get; set; }
-    public string Password { get; set; }
+public class CreateUsuarioDto
+{
+    public string Nombre;
+    public string Apellido;
+    public string Email;
+    public DateTime FechaNacimiento;
+    public string Password;
 }
 
-public class LoginDto {
-    public string Email { get; set; }
-    public string Password { get; set; }
+public class LoginDto
+{
+    public string Email;
+    public string Password;
 }
 
-public class GetUsuarioDto {
-    public string Nombre { get; set; }
-    public string Apellido { get; set; }
-    public string Email { get; set; }
+public class GetUsuarioDto
+{
+    public string Nombre;
+    public string Apellido;
+    public string Email;
 }
