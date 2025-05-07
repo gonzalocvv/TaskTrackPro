44ba87d7a5facf056f3eb1dfae8c0c41a4ab5c0e
gonzalocvv <gcabrera3210@gmail.com>
2025-05-07 17:46:15 -0300

    Dtos usuario

--- Dtos/Class1.cs
+++ Dtos/Class1.cs
@@ -1,5 +1,20 @@
 ﻿namespace Dtos;
 
-public class Class1
-{
+public class CreateUsuarioDto {
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Email { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string Password { get; set; }
+}
+
+public class LoginDto {
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class GetUsuarioDto {
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Email { get; set; }
 }
