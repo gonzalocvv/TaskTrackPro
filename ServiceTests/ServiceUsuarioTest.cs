8c62c39894b0a63aba6e3be4469f558fbdac6372
Juan Bautista Rey <jb269933@fi365.ort.edu.uy>
2025-05-09 14:12:31 -0300

    Arreglo referencias en la clase UsuarioServiceTest

---
+++ ServiceTests/ServiceUsuarioTest.cs
@@ -0,0 +1,15 @@
+using DataAccess;
+using Dominio;
+using Dtos;
+using Service;
+
+namespace ServiceTests;
+
+[TestClass]
+public class ServiceUsuarioTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
