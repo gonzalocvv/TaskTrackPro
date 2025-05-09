a462eae73373f37d9aada42bb84956e5c8e28980
Nicolas Ruy Lopez <NR256563@fi365.ort.edu.uy>
2025-05-09 17:40:02 -0300

    Red test crear usuario service

--- ServicesTests/UsuarioServicesTest.cs
+++ ServicesTests/UsuarioServicesTest.cs
@@ -1,10 +1,22 @@
+using Servicios;
+
 namespace ServicesTests;
 
 [TestClass]
 public class UsuarioServicesTest
 {
     [TestMethod]
-    public void TestMethod1()
+    public void CrearUsuarioTests()
     {
+        string nombre = "Nicolas";
+        string apellido = "Ruy Lopez";
+        string email = "nicolas@gmail.com";
+        DateTime fechaNacimiento = new DateTime(2020, 12, 23);
+        string contraseña = "Ab123456789!";
+        UsuarioService service = new UsuarioService();
+        var result = service.CrearUsuario(nombre, apellido, email, fechaNacimiento, contraseña);
+        Assert.AreEqual(result.Nombre, nombre);
+
+
     }
 }
--- Servicios/UsuarioService.cs
+++ Servicios/UsuarioService.cs
@@ -1,5 +1,11 @@
-﻿namespace Servicios;
+﻿using Dominio;
+
+namespace Servicios;
 
 public class UsuarioService
 {
+    public Usuario CrearUsuario(string nombre, string apellido, string email, DateTime fechaNacimiento, string contraseña)
+    {
+        throw new NotImplementedException();
+    }
 }
