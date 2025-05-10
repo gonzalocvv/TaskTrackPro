1938f851e456ae32e992d160193d206ea11266ed
Nicolas Ruy Lopez <NR256563@fi365.ort.edu.uy>
2025-05-10 13:52:10 -0300

    Refactor

--- Servicios/UsuarioService.cs
+++ Servicios/UsuarioService.cs
@@ -7,9 +7,9 @@ public class UsuarioService
     private List<Usuario> _list = new List<Usuario>();
     public Usuario CrearUsuario(string nombre, string apellido, string email, DateTime fechaNacimiento, string contraseña)
     {
-        Usuario newUser = new Usuario(nombre, apellido, email, fechaNacimiento, contraseña);
-        _list.Add(newUser);
-        return newUser;
+        Usuario nuevoUsuario = new Usuario(nombre, apellido, email, fechaNacimiento, contraseña);
+        _list.Add(nuevoUsuario);
+        return nuevoUsuario;
     }
 
 
