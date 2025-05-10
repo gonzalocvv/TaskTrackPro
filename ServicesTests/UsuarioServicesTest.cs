0171d3bc766b05b8925d4b584c9801cf11083179
Nicolas Ruy Lopez <NR256563@fi365.ort.edu.uy>
2025-05-10 13:16:38 -0300

    Red test get usuario por nombre

--- ServicesTests/UsuarioServicesTest.cs
+++ ServicesTests/UsuarioServicesTest.cs
@@ -1,3 +1,4 @@
+using Dominio;
 using Servicios;
 
 namespace ServicesTests;
@@ -16,5 +17,19 @@ public class UsuarioServicesTest
         UsuarioService service = new UsuarioService();
         var result = service.CrearUsuario(nombre, apellido, email, fechaNacimiento, contraseña);
         Assert.AreEqual(result.Nombre, nombre);
+    }
+
+    [TestMethod]
+    public void GetUsuarioPorNombreTest()
+    {
+        string nombre = "Nicolas";
+        string apellido = "Ruy Lopez";
+        string email = "nicolas@gmail.com";
+        DateTime fechaNacimiento = new DateTime(2020, 12, 23);
+        string contraseña = "Ab123456789!";
+        UsuarioService service = new UsuarioService();
+        service.CrearUsuario(nombre, apellido, email, fechaNacimiento, contraseña);
+        Usuario result = service.GetUsuarioPorNombre(nombre);
+
     }
 }
--- Servicios/UsuarioService.cs
+++ Servicios/UsuarioService.cs
@@ -4,10 +4,16 @@ namespace Servicios;
 
 public class UsuarioService
 {
+    private List<Usuario> _list = new List<Usuario>();
     public Usuario CrearUsuario(string nombre, string apellido, string email, DateTime fechaNacimiento, string contraseña)
     {
         Usuario u = new Usuario(nombre, apellido, email, fechaNacimiento, contraseña);
         return u;
     }
-    
+
+
+    public Usuario GetUsuarioPorNombre(string nombre)
+    {
+        throw new NotImplementedException();
+    }
 }
