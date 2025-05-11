153ed6486ace48c07b60ce5dfd2af5e746c8c959
Nicolas Ruy Lopez <NR256563@fi365.ort.edu.uy>
2025-05-11 13:06:32 -0300

    Red test agregar proyecto

---
+++ Servicios/ProyectoService.cs
@@ -0,0 +1,11 @@
+using Dominio;
+
+namespace Servicios;
+
+public class ProyectoService
+{
+    public Proyecto CrearProyecto(string nombre, string descripcion, DateTime fechaInicio, Usuario administradorP)
+    {
+        throw new NotImplementedException();
+    }
+}
