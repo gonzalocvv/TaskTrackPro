3d0a6450ecdf6303e9fdd1f75f87f206c3a6872a
Nicolas Ruy Lopez <NR256563@fi365.ort.edu.uy>
2025-05-11 13:06:32 -0300

    Red test agregar proyecto

---
+++ ServicesTests/ProyectoServicesTest.cs
@@ -0,0 +1,21 @@
+using Dominio;
+using Servicios;
+
+namespace ServicesTests;
+
+[TestClass]
+public class ProyectoServicesTest
+{
+    [TestMethod]
+    public void CrearProyectoTest()
+    {
+        string nombre = "Proyecto 1";
+        string descripcion = "Descripcion del proyecto 1";
+        DateTime fechaInicio = new DateTime(2025, 10, 1);
+        Usuario administradorP = new Usuario("Admin", "Admin", "admin@gmail.com", new DateTime(1990, 1, 1), "Admin123!");
+        ProyectoService service = new ProyectoService();
+        var result = service.CrearProyecto(nombre, descripcion, fechaInicio, administradorP);
+        Assert.AreEqual(result.Nombre, nombre);
+
+    }
+}
