455a76d6891ea4de8752020360a3e9f8c2f6dee6
Juan Bautista Rey <jb269933@fi365.ort.edu.uy>
2025-05-15 04:51:30 -0300

    ärregelos git conflictos

--- DominioTests/ProyectoTests.cs
+++ DominioTests/ProyectoTests.cs
@@ -1,26 +1,44 @@
 using Dominio;
+using Dtos;
 
 namespace DominioTests;
 
 [TestClass]
 public class ProyectoTests
 {
-    DateTime fechaInicioCorrecta = new DateTime(2025, 09, 08);
-    static DateTime fechaNac = new DateTime(2004, 9, 7);
-    Usuario admin = new Usuario("admin", "administrador", "administrador@admin.com", fechaNac, "Administrador@123");
+    private DateTime _fechaInicioCorrecta;
+    private DateTime _fechaNac;
+    private Usuario _admin;
+    private Proyecto _proyecto;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _fechaInicioCorrecta = new DateTime(2025, 09, 08);
+        _fechaNac = new DateTime(2004, 9, 7);
+        _admin = new Usuario(new CreateUsuarioDto
+        {
+            Nombre = "admin",
+            Apellido = "administrador",
+            Email = "administrador@admin.com",
+            FechaNacimiento = _fechaNac,
+            Contraseña = "Administrador@123"
+        });
+        _proyecto = new Proyecto("Limpieza", "Descripción válida", _fechaInicioCorrecta, _admin);
+    }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void ProyectoNombreVacioExcepcionTest()
     {
-        var proyecto = new Proyecto("", "En este proyecto se tiene como objetivo limpiar el salon", fechaInicioCorrecta, admin);
+        var proyecto = new Proyecto("", "En este proyecto se tiene como objetivo limpiar el salón", _fechaInicioCorrecta, _admin);
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void ProyectoDescripcionVacioExcepcionTest()
     {
-        var proyecto = new Proyecto("Limpieza", "", fechaInicioCorrecta, admin);
+        var proyecto = new Proyecto("Limpieza", "", _fechaInicioCorrecta, _admin);
     }
 
     [TestMethod]
@@ -28,38 +46,42 @@ public class ProyectoTests
     public void ProyectoDescripcionMasDe400CaracteresExcepcionTest()
     {
         string descripcion = new string('a', 401);
-        var proyecto = new Proyecto("Limpieza", descripcion, fechaInicioCorrecta, admin);
+        var proyecto = new Proyecto("Limpieza", descripcion, _fechaInicioCorrecta, _admin);
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void ProyectoFechaInicioAnteriorExcepcionTest()
     {
-        var proyecto = new Proyecto("Limpieza", "En este proyecto se tiene como objetivo limpiar el salon", DateTime.MinValue, admin);
+        var proyecto = new Proyecto("Limpieza", "En este proyecto se tiene como objetivo limpiar el salón", DateTime.MinValue, _admin);
     }
 
     [TestMethod]
     public void ProyectoAgregarMiembroTest()
     {
-        var proyecto = new Proyecto("Limpieza", "Descripción válida", fechaInicioCorrecta, admin);
-        var usuario = new Usuario("Gonzalo","Cabrera", "gonzalo@ejemplo.com", fechaNac, "Gonzalo9@");
-        proyecto.AgregarMiembro(usuario);
+        var usuario = new Usuario(new CreateUsuarioDto
+        {
+            Nombre = "Gonzalo",
+            Apellido = "Cabrera",
+            Email = "gonzalo@ejemplo.com",
+            FechaNacimiento = _fechaNac,
+            Contraseña = "Gonzalo9@"
+        });
+        _proyecto.AgregarMiembro(usuario);
 
-        Assert.IsTrue(proyecto.MiembrosProyecto.Contains(usuario));
+        Assert.IsTrue(_proyecto.MiembrosProyecto.Contains(usuario));
     }
 
     [TestMethod]
     [ExpectedException(typeof(InvalidOperationException))]
     public void ProyectoRemoverAdministradorExcepcionTest()
     {
-        var proyecto = new Proyecto("Limpieza", "Descripción válida", fechaInicioCorrecta, admin);
-        proyecto.RemoverMiembro(admin);
+        _proyecto.RemoverMiembro(_admin);
     }
 
     [TestMethod]
     public void ProyectoAdministradorEsMiembroPorDefectoTest()
     {
-        var proyecto = new Proyecto("Limpieza", "Descripción válida", fechaInicioCorrecta, admin);
-        Assert.IsTrue(proyecto.MiembrosProyecto.Contains(admin));
+        Assert.IsTrue(_proyecto.MiembrosProyecto.Contains(_admin));
     }
 }
--- ServicesTests/ProyectoServicesTest.cs
+++ ServicesTests/ProyectoServicesTest.cs
@@ -8,48 +8,56 @@ namespace ServicesTests;
 [TestClass]
 public class ProyectoServicesTest
 {
+    private MemoryDB _db;
+    private ProyectoService _service;
+    private Usuario _administradorP;
+    private CrearProyectoDto _proyectoDto;
+    
+    [TestInitialize]
+    public void SetUp()
+    {
+        _db = new MemoryDB();
+        _service = new ProyectoService(_db);
+
+        _administradorP = new Usuario(new CreateUsuarioDto
+        {
+            Nombre = "Admin",
+            Apellido = "Admin",
+            Email = "admin@gmail.com",
+            FechaNacimiento = new DateTime(1990, 1, 1),
+            Contraseña = "Admin123!"
+        });
+
+        _db.AgregarUsuario(_administradorP);
+
+        _proyectoDto = new CrearProyectoDto
+        {
+            Nombre = "Proyecto 1",
+            Descripcion = "Descripcion del proyecto 1",
+            FechaInicio = new DateTime(2025, 10, 1),
+            AdministradorEmail = _administradorP.Email
+        };
+    }
+
+    
     [TestMethod]
     public void CrearProyectoTest()
     {
-        string nombre = "Proyecto 1";
-        string descripcion = "Descripcion del proyecto 1";
-        DateTime fechaInicio = new DateTime(2025, 10, 1);
-        Usuario administradorP = new Usuario("Admin", "Admin", "admin@gmail.com", new DateTime(1990, 1, 1), "Admin123!");
-        MemoryDB db = new MemoryDB();
-        ProyectoService service = new ProyectoService(db);
-        db.AgregarUsuario(administradorP);
-        CrearProyectoDto proyectoDto = new CrearProyectoDto
-        {
-            Nombre = nombre,
-            Descripcion = descripcion,
-            FechaInicio = fechaInicio,
-            AdministradorEmail = administradorP.Email
-        };
-        var result = service.CrearProyecto(proyectoDto);
-        Assert.AreEqual(result.Nombre, nombre);
+        var result = _service.CrearProyecto(_proyectoDto);
 
+        Assert.AreEqual(result.Nombre, _proyectoDto.Nombre);
+        Assert.AreEqual(result.Descripcion, _proyectoDto.Descripcion);
+        Assert.AreEqual(result.FechaInicio, _proyectoDto.FechaInicio);
+        Assert.AreEqual(result.AdministradorP.Email, _proyectoDto.AdministradorEmail);
     }
 
     [TestMethod]
     public void GetProyectoPorNombreTest()
     {
-        string nombre = "Proyecto 1";
-        string descripcion = "Descripcion del proyecto 1";
-        DateTime fechaInicio = new DateTime(2025, 10, 1);
-        Usuario administradorP = new Usuario("Admin", "Admin", "admin@gmail.com", new DateTime(1990, 1, 1), "Admin123!");
-        MemoryDB db = new MemoryDB();
-        ProyectoService service = new ProyectoService(db);
-        CrearProyectoDto proyectoDto = new CrearProyectoDto
-        {
-            Nombre = nombre,
-            Descripcion = descripcion,
-            FechaInicio = fechaInicio,
-            AdministradorEmail = administradorP.Email
-        };
-        db.AgregarUsuario(administradorP);
-        service.CrearProyecto(proyectoDto);
-        Proyecto result = service.GetProyectoPorNombre(nombre);
-        Assert.AreEqual(result.Nombre, nombre);
+        _service.CrearProyecto(_proyectoDto);
+        Proyecto result = _service.GetProyectoPorNombre(_proyectoDto.Nombre);
+
+        Assert.AreEqual(result.Nombre, _proyectoDto.Nombre);
     }
 
     [TestMethod]
