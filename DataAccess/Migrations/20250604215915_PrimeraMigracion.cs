94a1671fe4e5bef255a3b210968a3c1bd0479322
NicoRLL <NR256563@fi365.ort.edu.uy>
2025-06-10 19:42:15 -0300

    Arreglos Migracion y Agrego Usuario

--- DataAccess/Migrations/20250604215915_PrimeraMigracion.cs
+++ DataAccess/Migrations/20250604215915_PrimeraMigracion.cs
@@ -11,6 +11,23 @@ namespace TaskTrackPro.Backend.DataAccess.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Email);
+                });
+
+            
             migrationBuilder.CreateTable(
                 name: "Proyectos",
                 columns: table => new
@@ -23,8 +40,15 @@ namespace TaskTrackPro.Backend.DataAccess.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Proyectos", x => x.Nombre);
+                    table.ForeignKey(
+                        name: "FK_Proyectos_Usuarios_AdministradorPEmail",
+                        column: x => x.AdministradorPEmail,
+                        principalTable: "Usuarios",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Cascade);
                 });
 
+            
             migrationBuilder.CreateTable(
                 name: "Tareas",
                 columns: table => new
@@ -47,71 +71,45 @@ namespace TaskTrackPro.Backend.DataAccess.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            
             migrationBuilder.CreateTable(
-                name: "TareaTarea",
+                name: "DependenciasTareas",
                 columns: table => new
                 {
-                    TareasQueDependenDeMiTitulo = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    TareasQueYoDependoTitulo = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                    TareaPrincipalTitulo = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    TareaDependienteTitulo = table.Column<string>(type: "nvarchar(450)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_TareaTarea", x => new { x.TareasQueDependenDeMiTitulo, x.TareasQueYoDependoTitulo });
+                    table.PrimaryKey("PK_DependenciasTareas", x => new { x.TareaPrincipalTitulo, x.TareaDependienteTitulo });
                     table.ForeignKey(
-                        name: "FK_TareaTarea_Tareas_TareasQueDependenDeMiTitulo",
-                        column: x => x.TareasQueDependenDeMiTitulo,
+                        name: "FK_DependenciasTareas_Tareas_TareaPrincipalTitulo",
+                        column: x => x.TareaPrincipalTitulo,
                         principalTable: "Tareas",
                         principalColumn: "Titulo",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "FK_TareaTarea_Tareas_TareasQueYoDependoTitulo",
-                        column: x => x.TareasQueYoDependoTitulo,
+                        name: "FK_DependenciasTareas_Tareas_TareaDependienteTitulo",
+                        column: x => x.TareaDependienteTitulo,
                         principalTable: "Tareas",
                         principalColumn: "Titulo");
                 });
 
-            migrationBuilder.CreateTable(
-                name: "Usuarios",
-                columns: table => new
-                {
-                    Email = table.Column<string>(type: "nvarchar(450)", nullable: false),
-                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Contraseña = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    ProyectoNombre = table.Column<string>(type: "nvarchar(450)", nullable: true),
-                    TareaTitulo = table.Column<string>(type: "nvarchar(450)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Usuarios", x => x.Email);
-                    table.ForeignKey(
-                        name: "FK_Usuarios_Proyectos_ProyectoNombre",
-                        column: x => x.ProyectoNombre,
-                        principalTable: "Proyectos",
-                        principalColumn: "Nombre");
-                    table.ForeignKey(
-                        name: "FK_Usuarios_Tareas_TareaTitulo",
-                        column: x => x.TareaTitulo,
-                        principalTable: "Tareas",
-                        principalColumn: "Titulo");
-                });
+            
+            migrationBuilder.AddColumn<string>(
+                name: "ProyectoNombre",
+                table: "Usuarios",
+                type: "nvarchar(450)",
+                nullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Proyectos_AdministradorPEmail",
-                table: "Proyectos",
-                column: "AdministradorPEmail");
+            
+            migrationBuilder.AddColumn<string>(
+                name: "TareaTitulo",
+                table: "Usuarios",
+                type: "nvarchar(450)",
+                nullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Tareas_ProyectoNombre",
-                table: "Tareas",
-                column: "ProyectoNombre");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_TareaTarea_TareasQueYoDependoTitulo",
-                table: "TareaTarea",
-                column: "TareasQueYoDependoTitulo");
-
+            
             migrationBuilder.CreateIndex(
                 name: "IX_Usuarios_ProyectoNombre",
                 table: "Usuarios",
@@ -123,32 +121,68 @@ namespace TaskTrackPro.Backend.DataAccess.Migrations
                 column: "TareaTitulo");
 
             migrationBuilder.AddForeignKey(
-                name: "FK_Proyectos_Usuarios_AdministradorPEmail",
+                name: "FK_Usuarios_Proyectos_ProyectoNombre",
+                table: "Usuarios",
+                column: "ProyectoNombre",
+                principalTable: "Proyectos",
+                principalColumn: "Nombre");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuarios_Tareas_TareaTitulo",
+                table: "Usuarios",
+                column: "TareaTitulo",
+                principalTable: "Tareas",
+                principalColumn: "Titulo");
+
+            
+            migrationBuilder.CreateIndex(
+                name: "IX_Proyectos_AdministradorPEmail",
                 table: "Proyectos",
-                column: "AdministradorPEmail",
-                principalTable: "Usuarios",
-                principalColumn: "Email",
-                onDelete: ReferentialAction.Cascade);
+                column: "AdministradorPEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tareas_ProyectoNombre",
+                table: "Tareas",
+                column: "ProyectoNombre");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DependenciasTareas_TareaDependienteTitulo",
+                table: "DependenciasTareas",
+                column: "TareaDependienteTitulo");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropForeignKey(
-                name: "FK_Proyectos_Usuarios_AdministradorPEmail",
-                table: "Proyectos");
+                name: "FK_Usuarios_Proyectos_ProyectoNombre",
+                table: "Usuarios");
 
-            migrationBuilder.DropTable(
-                name: "TareaTarea");
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuarios_Tareas_TareaTitulo",
+                table: "Usuarios");
 
+            migrationBuilder.DropColumn(
+                name: "ProyectoNombre",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "TareaTitulo",
+                table: "Usuarios");
+
+            
+            
             migrationBuilder.DropTable(
-                name: "Usuarios");
+                name: "DependenciasTareas");
 
             migrationBuilder.DropTable(
                 name: "Tareas");
 
             migrationBuilder.DropTable(
                 name: "Proyectos");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
         }
     }
 }
