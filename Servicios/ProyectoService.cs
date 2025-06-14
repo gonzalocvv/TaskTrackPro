7a0dbc60be50b84d16853c5d30d30f7beb0e66b9
gonzalocvv <gcabrera3210@gmail.com>
2025-06-14 19:54:35 -0300

    RED GetProyectoPorNombre

--- Servicios/ProyectoService.cs
+++ Servicios/ProyectoService.cs
@@ -32,7 +32,7 @@ public class ProyectoService
 
     public Proyecto GetProyectoPorNombre(string nombre)
     {
-        var proyectoParaDevolver = _db.GetListaProyectosPorNombre(nombre);
+        var proyectoParaDevolver = _proyectoRepository.GetListaProyectosPorNombre(nombre);
         if (proyectoParaDevolver == null)
         {
             throw new ArgumentNullException("El proyecto no existe");
